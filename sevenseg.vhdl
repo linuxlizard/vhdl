a2795db317ae9900de2c1c241c04e00d8ba81bd4
David Poole <testcluster@gmail.com>
2012-10-06 15:06:53 -0600

    remove the BCD conversion to own module (only encodes 7seg)

--- sevenseg.vhdl
+++ sevenseg.vhdl
@@ -1,4 +1,4 @@
--- Seven segment encode function with BCD conversion.
+-- Seven segment encode function.
 --
 -- Started point was SimpleSsegLedDemo.vhd from Digilent's UserDemo
 --
@@ -7,8 +7,8 @@
 
 library ieee;
 use ieee.std_logic_1164.ALL;
-use ieee.std_logic_arith.ALL;
-use ieee.std_logic_unsigned.ALL;
+--use ieee.std_logic_arith.ALL;
+--use ieee.std_logic_unsigned.ALL;
 
 entity SevenSegmentEncoder is
 
@@ -21,41 +21,6 @@ entity SevenSegmentEncoder is
 end SevenSegmentEncoder;
 
 architecture Behavioral of SevenSegmentEncoder is
-
-    -- http://vhdlguru.blogspot.com/2010/04/8-bit-binary-to-bcd-converter-double.html
-    function to_bcd ( bin : std_logic_vector(7 downto 0) ) return std_logic_vector is
-
---    use ieee.std_logic_arith.all;
---    use ieee.std_logic_unsigned.all;
-
-    variable i : integer:=0;
-    variable bcd : std_logic_vector(11 downto 0) := (others => '0');
-    variable bint : std_logic_vector(7 downto 0) := bin;
-
-    begin
-        for i in 0 to 7 loop  -- repeating 8 times.
-            bcd(11 downto 1) := bcd(10 downto 0);  --shifting the bits.
-            bcd(0) := bint(7);
-            bint(7 downto 1) := bint(6 downto 0);
-            bint(0) :='0';
-
-
-            if(i < 7 and bcd(3 downto 0) > "0100") then --add 3 if BCD digit is greater than 4.
-                bcd(3 downto 0) := bcd(3 downto 0) + "0011";
-            end if;
-
-            if(i < 7 and bcd(7 downto 4) > "0100") then --add 3 if BCD digit is greater than 4.
-                bcd(7 downto 4) := bcd(7 downto 4) + "0011";
-            end if;
-
-            if(i < 7 and bcd(11 downto 8) > "0100") then  --add 3 if BCD digit is greater than 4.
-                bcd(11 downto 8) := bcd(11 downto 8) + "0011";
-            end if;
-
-        end loop;
-        return bcd;
-
-    end to_bcd;
 
 begin
   --HEX-to-seven-segment decoder
@@ -91,19 +56,11 @@ begin
  
  
     calculate_7seg : process(ck) is
-        variable num : std_logic_vector(7 downto 0 );
-        variable tmp : std_logic_vector( 11 downto 0 );
-        variable tmp2 : std_logic_vector( 3 downto 0 );
     begin
-        if rising_edge(ck) then 
-            -- unroll it due to inexperience with VHDL
-            num := ('0','0','0','0',nibble(3),nibble(2),nibble(1),nibble(0));
-            --num := ('0','0','0','0',cntDiv(3),cntDiv(2),cntDiv(1),cntDiv(0));
-            --num := ('0','0','0','0',cntDisp(3),cntDisp(2),cntDisp(1),cntDisp(0));
-            tmp := to_bcd(num);
-            tmp2 := (tmp(3),tmp(2),tmp(1),tmp(0));
-            
-            case tmp2 is 
+        if rst='1' then
+            -- todo?
+        elsif rising_edge(ck) then 
+            case nibble is 
                 when "0001" => -- 1
                     seg <= "1111001";
                 when "0010" => -- 2
@@ -122,6 +79,18 @@ begin
                     seg <= "0000000";
                 when "1001" => -- 9
                     seg <= "0010000";
+                when "1010" => -- 0x0a d'10
+                    seg <= "0001000";
+                when "1011" => -- 0x0b d'11
+                    seg <= "0000011";  
+                when "1100" => -- 0x0c d'12
+                    seg <= "1000110";
+                when "1101" => -- 0x0d d'13
+                    seg <= "0100001";
+                when "1110" => -- 0x0e d'14
+                    seg <= "0000110";
+                when "1111" => -- 0x0f d'15
+                    seg <= "0001110";
                 when others => -- 0
                     seg <= "1000000";
             end case;
