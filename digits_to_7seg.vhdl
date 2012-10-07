79515efa8d773aeb92b6c2369c5a485b0698fdf2
David Poole <testcluster@gmail.com>
2012-10-07 09:30:12 -0600

    add internal signal to hold intermediate result

--- digits_to_7seg.vhdl
+++ digits_to_7seg.vhdl
@@ -14,7 +14,6 @@ use ieee.std_logic_textio.all;
 entity digits_to_7seg is
     -- signals in Basys2
     port(  mclk : in std_logic;
-         byte_in : in std_logic_vector(7 downto 0 );
 
             -- 7seg display
             seg : out std_logic_vector(6 downto 0 );
@@ -28,6 +27,8 @@ entity digits_to_7seg is
 end entity digits_to_7seg;
 
 architecture run_digits_to_7seg of digits_to_7seg is
+
+    signal byte_in : std_logic_vector(7 downto 0 );
 
     -- bcd out to 7seg encoder in
     signal bcd_outnibble0 : std_logic_vector (3 downto 0);
@@ -88,10 +89,13 @@ begin
     -- hardwire decimal point for now
     dp <= '0';
 
+    byte_in <= "11111111";
+--    byte_in <= "00101010"; -- d'42
+
     -- the actual divider will be 2.1e6 or so (25Mhz down to 15hz)
     run_divider : clk_divider
-        generic map(clkmax => 4) -- simulation
---        generic map(clkmax =>  21000000) -- synthesis
+--        generic map(clkmax => 4) -- simulation
+        generic map(clkmax => 50000) -- synthesis
         port map( clk_in => mclk,
                 reset => rst,
                 clk_out => divider_out_7segmuxor_in );
@@ -111,14 +115,14 @@ begin
     sevenseg_digit3 : SevenSegmentEncoder 
         port map ( rst => rst,
                     ck => mclk,
-                    nibble => "0000",
+                    nibble => "0011",
                     seg => out7seg3
                 );
-
 
     sevenseg_digit2 : SevenSegmentEncoder
         port map ( rst => rst,
                     ck => mclk,
+--                    nibble => "0010",
                     nibble => bcd_outnibble2,
                     seg => out7seg2 
                 );
@@ -126,6 +130,7 @@ begin
     sevenseg_digit1 : SevenSegmentEncoder
         port map ( rst => rst,
                     ck => mclk,
+--                    nibble => "0001",
                     nibble => bcd_outnibble1,
                     seg => out7seg1 
                 );
@@ -133,6 +138,7 @@ begin
     sevenseg_digit0 : SevenSegmentEncoder
         port map ( rst => rst,
                     ck => mclk,
+--                    nibble => "0000",
                     nibble => bcd_outnibble0,
                     seg => out7seg0 
                 );
@@ -144,6 +150,10 @@ begin
                     digit_1 => out7seg1,
                     digit_2 => out7seg2,
                     digit_3 => out7seg3,
+--                    digit_0 => "1111001",
+--                    digit_1 => "0100100",
+--                    digit_2 => "0110000",
+--                    digit_3 => "0011001",
                     anode_out => an, -- 7segment display anode
                     digit_out => seg -- 7segment display segment
                 );
