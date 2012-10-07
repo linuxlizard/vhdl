8a5b271239791d859f3d587898dfbabddaf35286
David Poole <testcluster@gmail.com>
2012-10-07 12:35:29 -0600

    dip switches to 7seg ; testing communicating switches -> registers ->7seg

---
+++ switch_to_7seg.vhdl
@@ -0,0 +1,128 @@
+-- Send switches to LEDs using button presses as the assignment requires.
+--
+-- Currently for testing internal components. Will synthesize, too.
+--
+-- David Poole 03-Oct-2012
+
+library ieee;
+use ieee.std_logic_1164.ALL;
+use ieee.numeric_std.all;
+use std.textio.all;
+use ieee.std_logic_textio.all;
+
+entity switch_to_7seg is 
+    port(  mclk : in std_logic;
+            btn : in std_logic_vector(3 downto 0);
+             sw : in std_logic_vector(7 downto 0);
+            seg : out std_logic_vector( 6 downto 0 );
+             an : out std_logic_vector( 3 downto 0 )
+        ); 
+end entity switch_to_7seg;
+
+architecture switch_to_7seg_arch of switch_to_7seg is
+
+    signal rst : std_logic;
+	 
+    component clk_divider is
+        generic (clkmax : integer);
+        port ( reset : in std_logic;
+               clk_in : in std_logic;
+               clk_out : out std_logic );
+    end component clk_divider;
+
+    component d_register is
+        generic (width : integer);
+        port (clk : in std_logic;
+              reset : in std_logic := '1';
+              input_enable : in std_logic;
+              output_enable : in std_logic;
+              data_in : in std_logic_vector( width-1 downto 0 );
+              data_out : out std_logic_vector( width-1 downto 0 )
+        );
+    end component d_register;
+
+    component regrotate is
+        port ( reset : in std_logic;
+               clk : in std_logic;
+              reg1 : in std_logic_vector( 7 downto 0 );
+              reg2 : in std_logic_vector( 7 downto 0 );
+              reg_out : out std_logic_vector( 7 downto 0 ) ;
+              which_out : out std_logic
+             );
+    end component regrotate;
+
+    component digits_to_7seg is
+        port(  mclk : in std_logic;
+                byte_in : in std_logic_vector(7 downto 0);
+                seg : out std_logic_vector(6 downto 0 );
+                an : out std_logic_vector(3 downto 0);
+                dp : out std_logic
+            ); 
+    end component digits_to_7seg;
+
+    signal reg_num : std_logic;
+
+    signal rotater_clock : std_logic;
+
+    signal reg1_output : std_logic_vector( 7 downto 0 ) := (others=>'0');
+    signal reg1_in_en : std_logic;
+    signal reg1_out_en : std_logic;
+
+    signal reg2_output : std_logic_vector( 7 downto 0 ) := (others=>'0');
+    signal reg2_in_en : std_logic;
+    signal reg2_out_en : std_logic;
+
+    -- from register rotater to 7-seg displayer
+    signal reg_rotate_out_7seg_in : std_logic_vector (7 downto 0) := (others=>'0');
+
+    signal dp : std_logic := '0';
+begin
+    -- future compatibility for an incoming reset signal
+    rst <= '0';
+
+    dp <= '0';
+	 
+    -- the actual divider will be 125e6 or so (25Mhz down to 0.20hz)
+    divider : clk_divider
+        generic map(clkmax => 4) -- simulation
+--        generic map(clkmax => 125000000) -- synthesis
+        port map( clk_in => mclk,
+                reset => rst,
+                clk_out => rotater_clock );
+
+    register_1 : d_register
+       generic map( width => 8)
+       port map( clk => mclk,
+                 reset => rst,
+                 input_enable => btn(0),
+                 output_enable => btn(3),
+                 data_in => sw,
+                 data_out => reg1_output);
+
+    register_2 : d_register
+       generic map( width => 8)
+       port map( clk => mclk,
+                 reset => rst,
+                 input_enable => btn(1),
+                 output_enable => btn(3),
+                 data_in => sw,
+                 data_out => reg2_output);
+
+    register_rotater : regrotate
+        port map( clk => rotater_clock,
+                  reset => rst,
+                  reg1 => reg1_output,
+                  reg2 => reg2_output,
+                  reg_out => reg_rotate_out_7seg_in,
+                  which_out => reg_num );
+
+    run_digits_to_7seg : digits_to_7seg
+        port map( mclk => mclk,
+                byte_in => reg_rotate_out_7seg_in,
+                    seg => seg,
+                    an => an,
+                    dp => dp
+                );
+
+end architecture switch_to_7seg_arch;
+
---
+++ test_switch_to_7seg.vhdl
@@ -0,0 +1,103 @@
+-- Simulation test bench replicating parts of Basys-2 board.
+--
+-- David Poole  03-Oct-2012
+
+library ieee;
+use ieee.std_logic_1164.ALL;
+use ieee.numeric_std.all;
+use std.textio.all;
+use ieee.std_logic_textio.all;
+
+entity test_switch_to_7seg is 
+end entity test_switch_to_7seg;
+
+architecture test_switch_to_7seg_arch of test_switch_to_7seg is 
+    -- use the same names as the actual hardware
+    signal mclk :  std_logic;
+    signal rst : std_logic;
+    signal btn: std_logic_vector(3 downto 0);
+    signal sw :  std_logic_vector(7 downto 0);
+    signal led: std_logic_vector(7 downto 0);
+    signal seg : std_logic_vector( 6 downto 0 );
+    signal an : std_logic_vector( 3 downto 0 );
+    signal dp : std_logic;
+
+    component switch_to_7seg is 
+        port(  mclk : in std_logic;
+                btn : in std_logic_vector(3 downto 0);
+                 sw : in std_logic_vector(7 downto 0);
+                seg : out std_logic_vector( 6 downto 0 );
+                 an : out std_logic_vector( 3 downto 0 )
+            ); 
+    end component switch_to_7seg;
+
+begin
+    run_switch_to_7seg : switch_to_7seg
+        port map( mclk => mclk,
+                  btn => btn,
+                  sw => sw,
+                  seg => seg,
+                  an => an
+                  );
+                  
+    clock : process is
+    begin
+       mclk <= '0'; wait for 10 ns;
+       mclk <= '1'; wait for 10 ns;
+    end process clock;
+
+    stimulus : process is
+        variable str : line;
+        variable i : integer;
+    begin
+        write( str, string'("hello, world") );
+        writeline( output, str );
+
+        rst <= '1';
+        btn <= "0000";
+        sw <= "00000000";
+        wait for 15 ns;
+
+        rst <= '0';
+        wait for 10 ns;
+
+        -- load register 1 with a value
+        sw <= "00000001";
+        btn <= "0001";  -- push button 0
+        wait for 10 ns;
+        write( str, string'("register1 loaded") );
+        writeline( output, str );
+
+        btn <= "0000"; -- release button 0
+        wait for 10 ns;
+
+        -- load register 2 with a value
+        sw <= "00000010";
+        btn <= "0010";  -- push button 1
+        wait for 10 ns;
+        write( str, string'("register2 loaded") );
+        writeline( output, str );
+
+        btn <= "0000"; -- release button 1
+        wait for 10 ns;
+
+        -- register1 and register2 should now hold values
+        -- push button 3 to output the registers' values to the rotater
+        btn <= "1000";
+        wait for 10 ns;
+        btn <= "0000";
+        wait for 10 ns;
+
+        -- so what do we have?
+        for i in 0 to 100 loop
+            work.debug_utils.dbg_7seg( seg, an, dp ); 
+            wait for 50 ns;
+        end loop;
+
+        rst <= '1';
+        wait for 10 ns;
+
+       wait;
+    end process stimulus;
+end architecture test_switch_to_7seg_arch;
+
