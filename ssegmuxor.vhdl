a2b6c595d063718772ec5d993d46e2ed38188e5c
David Poole <testcluster@gmail.com>
2012-10-07 12:38:05 -0600

    add internal signals for anode, digits

--- ssegmuxor.vhdl
+++ ssegmuxor.vhdl
@@ -22,7 +22,8 @@ end entity ssegmuxor;
 
 architecture clk_ssegmuxor_arch of ssegmuxor is
     signal counter : integer := 0;
-    signal save_digit_out : std_logic_vector(6 downto 0) := "0000000";
+    signal internal_anode_out : std_logic_vector(3 downto 0) := "0000";
+    signal internal_digit_out : std_logic_vector(6 downto 0) := "0000000";
 begin
     behavior : process(clk) is
     begin
@@ -31,32 +32,33 @@ begin
         elsif rising_edge(clk) then
             case counter is
                 when 0 => 
-                    anode_out <= "0111";
-                    save_digit_out <= digit_0;
---                    save_digit_out <= "1111001";  -- 1
+                    internal_anode_out <= "0111";
+                    internal_digit_out <= digit_0;
+--                    internal_digit_out <= "1111001";  -- 1
                     counter <= 1;
                 when 1 =>
-                    anode_out <= "1011";
-                    save_digit_out <= digit_1;
+                    internal_anode_out <= "1011";
+                    internal_digit_out <= digit_1;
 --                    digit_out <= "0100100";  -- 2
                     counter <= 2;
                 when 2 =>
-                    anode_out <= "1101";
-                    save_digit_out <= digit_2;
+                    internal_anode_out <= "1101";
+                    internal_digit_out <= digit_2;
 --                    digit_out <= "0110000";  -- 3
                     counter <= 3;
                 when 3 =>
-                    anode_out <= "1110";
-                    save_digit_out <= digit_3;
+                    internal_anode_out <= "1110";
+                    internal_digit_out <= digit_3;
 --                    digit_out <= "0011001";  -- 4
                     counter <= 0;
                 when others =>
-                    anode_out <= "0000";
-                    save_digit_out <= "1111111";
+                    internal_anode_out <= "0000";
+                    internal_digit_out <= "1111111";
             end case;
         end if;
 
-        digit_out <= save_digit_out;
+        digit_out <= internal_digit_out;
+        anode_out <= internal_anode_out;
     end process behavior;
 end architecture clk_ssegmuxor_arch;
 
