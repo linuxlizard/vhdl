cda38d599c09d683c494e96a9b5d6794f9f4a9dc
David Poole <testcluster@gmail.com>
2012-10-06 21:40:43 -0600

    renumber digits to start at zero

--- ssegmuxor.vhdl
+++ ssegmuxor.vhdl
@@ -6,23 +6,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity clk_ssegmuxor is
+entity ssegmuxor is
     port (  reset : in std_logic;
             clk : in std_logic;
+            digit_0 : in std_logic_vector (6 downto 0 );
             digit_1 : in std_logic_vector (6 downto 0 );
             digit_2 : in std_logic_vector (6 downto 0 );
             digit_3 : in std_logic_vector (6 downto 0 );
-            digit_4 : in std_logic_vector (6 downto 0 );
 
             anode_out : out std_logic_vector (3 downto 0 );
             digit_out : out std_logic_vector (6 downto 0 ) 
         );
 
-end entity clk_ssegmuxor;
+end entity ssegmuxor;
 
-architecture clk_ssegmuxor_arch of clk_ssegmuxor is
-    signal counter : integer;
-
+architecture clk_ssegmuxor_arch of ssegmuxor is
+    signal counter : integer := 0;
 begin
     behavior : process(clk) is
     begin
@@ -33,16 +32,16 @@ begin
             case counter is
                 when 0 => 
                     anode_out <= "0111";
-                    digit_out <= digit_1;
+                    digit_out <= digit_0;
                 when 1 =>
                     anode_out <= "1011";
-                    digit_out <= digit_2;
+                    digit_out <= digit_1;
                 when 2 =>
                     anode_out <= "1101";
-                    digit_out <= digit_3;
+                    digit_out <= digit_2;
                 when 3 =>
                     anode_out <= "1110";
-                    digit_out <= digit_4;
+                    digit_out <= digit_3;
                 when others =>
                     anode_out <= "0000";
                     digit_out <= "1111111";
