f3097817476972f35abb3684b273b2b0ce51e616
David Poole <testcluster@gmail.com>
2012-10-09 09:35:16 -0600

    add handy hex values to 7seg binary digits

--- dbg.vhdl
+++ dbg.vhdl
@@ -26,22 +26,22 @@ package body debug_utils is
     -- Handy for test/debug
     begin
         case sseg is 
-         when "1111001" => return 1;
-         when "0100100" => return 2;
-         when "0110000" => return 3;
-         when "0011001" => return 4;
-         when "0010010" => return 5;
-         when "0000010" => return 6;
-         when "1111000" => return 7;
-         when "0000000" => return 8;
-         when "0010000" => return 9;
+         when "1111001" => return 1; -- 0x79
+         when "0100100" => return 2; -- 0x24
+         when "0110000" => return 3; -- 0x30
+         when "0011001" => return 4; -- 0x29
+         when "0010010" => return 5; -- 0x12
+         when "0000010" => return 6; -- 0x02
+         when "1111000" => return 7; -- 0x78
+         when "0000000" => return 8; -- 0x00
+         when "0010000" => return 9; -- 0x10
          when "0001000" => return 16#A#;
          when "0000011" => return 16#b#;
          when "1000110" => return 16#C#;
          when "0100001" => return 16#d#;
          when "0000110" => return 16#E#;
          when "0001110" => return 16#F#;
-         when "1000000" => return 0;
+         when "1000000" => return 0; -- 0x40
          when others => return -1;            
         end case;
     end sevenseg_to_integer;
