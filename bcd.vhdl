5786c12f6cf2e012f81c30673cc8f59875daf4ee
David Poole <testcluster@gmail.com>
2012-10-17 22:02:53 -0600

    bump from 8-bit byte to 16-bit word

--- bcd.vhdl
+++ bcd.vhdl
@@ -12,8 +12,8 @@ use ieee.std_logic_unsigned.all;
 entity bcd_encoder is
     port (rst : in std_logic;
           clk : in std_logic;
-          byte_in : in std_logic_vector(7 downto 0 );
-          bcd_out : out std_logic_vector( 11 downto 0 );
+          word_in : in std_logic_vector(15 downto 0 );
+          bcd_out : out std_logic_vector( 19 downto 0 );
           negative_out : out std_logic
          );
 end entity bcd_encoder;
@@ -22,36 +22,44 @@ end entity bcd_encoder;
 architecture bcd_encoder_arch of bcd_encoder is 
 
     -- http://vhdlguru.blogspot.com/2010/04/8-bit-binary-to-bcd-converter-double.html
-    function to_bcd ( bin : std_logic_vector(7 downto 0) ) return std_logic_vector is
+    function to_bcd ( bin : std_logic_vector(15 downto 0) ) return std_logic_vector is
         variable i : integer:=0;
-        variable bcd : std_logic_vector(11 downto 0) := (others => '0');
-        variable bint : std_logic_vector(7 downto 0) := bin;
+        variable bcd : std_logic_vector(19 downto 0) := (others => '0');
+        variable bint : std_logic_vector(15 downto 0) := bin;
 
     begin
-        for i in 0 to 7 loop  -- repeating 8 times.
-            bcd(11 downto 1) := bcd(10 downto 0);  --shifting the bits.
-            bcd(0) := bint(7);
-            bint(7 downto 1) := bint(6 downto 0);
+        for i in 0 to 15 loop  -- repeating 8 times.
+            bcd(19 downto 1) := bcd(18 downto 0);  --shifting the bits.
+            bcd(0) := bint(15);
+            bint(15 downto 1) := bint(14 downto 0);
             bint(0) :='0';
 
 
-            if(i < 7 and bcd(3 downto 0) > "0100") then --add 3 if BCD digit is greater than 4.
+            if(i < 15 and bcd(3 downto 0) > "0100") then --add 3 if BCD digit is greater than 4.
                 bcd(3 downto 0) := bcd(3 downto 0) + "0011";
             end if;
 
-            if(i < 7 and bcd(7 downto 4) > "0100") then --add 3 if BCD digit is greater than 4.
+            if(i < 15 and bcd(7 downto 4) > "0100") then --add 3 if BCD digit is greater than 4.
                 bcd(7 downto 4) := bcd(7 downto 4) + "0011";
             end if;
 
-            if(i < 7 and bcd(11 downto 8) > "0100") then  --add 3 if BCD digit is greater than 4.
+            if(i < 15 and bcd(11 downto 8) > "0100") then  --add 3 if BCD digit is greater than 4.
                 bcd(11 downto 8) := bcd(11 downto 8) + "0011";
+            end if;
+
+            if(i < 15 and bcd(15 downto 12) > "0100") then  --add 3 if BCD digit is greater than 4.
+                bcd(15 downto 12) := bcd(15 downto 12) + "0011";
+            end if;
+
+            if(i < 15 and bcd(19 downto 16) > "0100") then  --add 3 if BCD digit is greater than 4.
+                bcd(19 downto 16) := bcd(19 downto 16) + "0011";
             end if;
         end loop;
 
         return bcd;
     end to_bcd;
 
-    signal internal_bcd_out : std_logic_vector(11 downto 0) := (others=>'0');
+    signal internal_bcd_out : std_logic_vector(19 downto 0) := (others=>'0');
     signal internal_negative_out : std_logic := '0';
     signal num : unsigned(7 downto 0) := (others=>'0');
 begin
@@ -74,7 +82,7 @@ begin
 --                internal_bcd_out <= to_bcd(byte_in);
 --                internal_negative_out <= '0';
 --            end if;
-            internal_bcd_out <= to_bcd(byte_in);
+            internal_bcd_out <= to_bcd(word_in);
             internal_negative_out <= '0';
 				
         end if;
--- test_bcd.vhdl
+++ test_bcd.vhdl
@@ -15,27 +15,27 @@ end entity test_bcd;
 architecture run_test_bcd of test_bcd is
     signal t_rst : std_logic := '1';
     signal t_clk : std_logic := '0';
-    signal t_byte_in : std_logic_vector( 7 downto 0 );
-    signal t_bcd_out : std_logic_vector( 11 downto 0 );
+    signal t_word_in : std_logic_vector( 15 downto 0 );
+    signal t_bcd_out : std_logic_vector( 19 downto 0 );
     signal t_negative_flag : std_logic;
 
     component bcd_encoder is
         port (rst : in std_logic;
               clk : in std_logic;
-              byte_in : in std_logic_vector(7 downto 0 );
-              bcd_out : out std_logic_vector( 11 downto 0 );
+              word_in : in std_logic_vector(15 downto 0 );
+              bcd_out : out std_logic_vector( 19 downto 0 );
               negative_out : out std_logic
              );
     end component bcd_encoder;
 
-    procedure dbg_bcd( byte_value : in std_logic_vector(7 downto 0 );
-                      bcd_value : in std_logic_vector(11 downto 0 );
+    procedure dbg_bcd( word_value : in std_logic_vector(15 downto 0 );
+                      bcd_value : in std_logic_vector(19 downto 0 ); 
                       negative_flag : in std_logic ) is
 
         variable str : line;
     begin
         write( str, string'("input=") );
-        hwrite( str, byte_value );
+        hwrite( str, word_value );
         write( str, string'(" output=") );
         hwrite( str, bcd_value );
         if negative_flag = '1' then
@@ -49,9 +49,9 @@ begin
     uut : bcd_encoder 
         port map( rst => t_rst,
                   clk => t_clk,
-                  byte_in => t_byte_in,
-                  bcd_out => t_bcd_out,
-                  negative_out => t_negative_flag
+                  word_in => t_word_in,
+                  bcd_out => t_bcd_out, 
+                  negative_out => t_negative_flag 
                 );
     
     clock : process is 
@@ -65,39 +65,38 @@ begin
         variable str : line;
     begin
         write( output, string'("hello, world") );
-        t_byte_in <= "00000000";
+        t_word_in <= std_logic_vector(to_unsigned(0,16)); 
         t_rst <= '1';
         wait for 15 ns;
 
-        t_byte_in <= "00000001";
+        t_word_in <= std_logic_vector(to_unsigned(1,16));
         t_rst <= '0';
         wait for 10 ns;
 
         wait for 10 ns;
-        dbg_bcd( t_byte_in, t_bcd_out, t_negative_flag );
+        dbg_bcd( t_word_in, t_bcd_out, t_negative_flag );
         wait for 10 ns;
 
-        t_byte_in <= "00001110";
+        t_word_in <= std_logic_vector(to_unsigned(7,16));
         wait for 10 ns;
-        dbg_bcd( t_byte_in, t_bcd_out, t_negative_flag );
-        wait for 10 ns;
-
-        -- negative one (-2) 
-        t_byte_in <= "11111110";
-        wait for 10 ns;
-        dbg_bcd( t_byte_in, t_bcd_out, t_negative_flag );
+        dbg_bcd( t_word_in, t_bcd_out, t_negative_flag );
         wait for 10 ns;
 
-        t_byte_in <= "10000000";
+        t_word_in <= std_logic_vector(to_unsigned(42,16)); 
         wait for 10 ns;
-        dbg_bcd( t_byte_in, t_bcd_out, t_negative_flag );
+        dbg_bcd( t_word_in, t_bcd_out, t_negative_flag );
+        wait for 10 ns;
+
+        t_word_in <= std_logic_vector(to_unsigned(32767,16));
+        wait for 10 ns;
+        dbg_bcd( t_word_in, t_bcd_out, t_negative_flag );
         wait for 10 ns;
 
         for i in 1 to 255 loop
-            t_byte_in <= std_logic_vector(to_unsigned(i,8));
+            t_word_in <= std_logic_vector(to_unsigned(i,16));
             wait for 10 ns;
 
-            dbg_bcd( t_byte_in, t_bcd_out, t_negative_flag );
+            dbg_bcd( t_word_in, t_bcd_out, t_negative_flag );
             wait for 10 ns;
         end loop;
 
