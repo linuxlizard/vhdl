80c4fad6418624a8ff65fe40ba2ceaccac632472
David Poole <testcluster@gmail.com>
2012-10-20 17:58:59 -0600

    first test: Push $1 three times. Select Zone A. One ticket. Should dispense ticket and show $2 change

--- basys2.vhdl
+++ basys2.vhdl
@@ -45,6 +45,20 @@ architecture basys2_arch of basys2 is
         work.debug_utils.dbg_7seg( seg, an, dp );
     end;
 
+    function add_coin ( value : in integer ) return std_logic_vector is 
+    begin
+        if value=10 then
+            return "0100";
+        elsif value=25 then
+            return "0010";
+        elsif value=100 then
+            return "0001";
+        else 
+            -- TODO halt somehow
+            return "1111";
+        end if;
+    end;
+
 begin
 
     run_subway_tickets : subway_tickets
@@ -74,8 +88,51 @@ begin
         sw <= "00000001";
         wait for 55 ns;
 
+        -- clear reset
         sw <= "00000000";
-        wait for 10 ns;
+        wait for 80 ns;
+
+        -- insert $3
+        btn <= add_coin( 100 );
+        wait for 20 ns;
+        btn <= "0000";
+        wait for 80 ns;
+
+        btn <= add_coin( 100 );
+        wait for 20 ns;
+        btn <= "0000";
+        wait for 80 ns;
+
+        btn <= add_coin( 100 );
+        wait for 20 ns;
+        btn <= "0000";
+        wait for 80 ns;
+
+        btn <= add_coin( 100 );
+        wait for 20 ns;
+        btn <= "0000";
+        wait for 80 ns;
+
+        -- done entering money; push & release btn3 to go to zone chooser
+        btn <= "1000";
+        wait for 80 ns;
+        btn <= "0000";
+        wait for 80 ns;
+
+
+        -- push & release btn 3 to go to next state (we'll go to zone A)
+        btn <= "1000";
+        wait for 80 ns;
+        btn <= "0000";
+        wait for 80 ns;
+
+        --push & release btn 3 to indicate we want one ticket (the default)
+        btn <= "1000";
+        wait for 80 ns;
+        btn <= "0000";
+        wait for 80 ns;
+
+        -- should be dispensing tickets now
 
         -- so what do we have?
         for i in 0 to 20000 loop 
@@ -88,7 +145,8 @@ begin
             -- clock period is 20ns. 7seg divider is 4 so 7seg period is 80ns ;
             -- sample @ Nyquist just so I can show off I know what Nyquist
             -- means
-            wait for 40 ns;
+--            wait for 40 ns;
+            wait for 1 us;
         end loop;
 
         wait for 10 ns;
