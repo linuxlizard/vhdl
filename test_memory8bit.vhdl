1d59cca40a81a431c9e6230d19023708eb4be78f
David Poole <testcluster@gmail.com>
2012-11-18 20:45:47 -0700

    add read and write signals;

--- test_memory8bit.vhdl
+++ test_memory8bit.vhdl
@@ -17,12 +17,13 @@ architecture test_memory8bit_arch of test_memory8bit is
     signal mclk :  std_logic := '0';
     
     signal t_write_en : std_logic := '0';
-    signal t_addr : unsigned(7 downto 0 );
+    signal t_read_addr : unsigned(7 downto 0 );
+    signal t_write_addr : unsigned(7 downto 0 );
     signal t_data_in : unsigned( 7 downto 0 );
     signal t_data_out : unsigned( 7 downto 0 );
 
     component memory8bit is
-        generic ( width : integer; depth : integer );
+        generic ( depth : integer );
         port ( clk : in std_logic;
                write_enable : in std_logic;
                write_addr : in unsigned(7 downto 0 );
@@ -43,8 +44,8 @@ begin
         generic map( depth => 32 )
         port map ( clk => mclk,
                    write_enable => t_write_en,
-                   write_addr => t_addr,
-                   read_addr => t_addr,
+                   write_addr => t_write_addr,
+                   read_addr => t_read_addr,
                    data_in => t_data_in,
                    data_out => t_data_out
                 );
@@ -64,9 +65,10 @@ begin
         write( str, string'("hello, world") );
         writeline( output, str );
 
+        t_read_addr <= X"00";
         wait for 5 ns;
 
-        t_addr <= X"01";
+        t_write_addr <= X"01";
         t_data_in <= X"ee";
         t_write_en <= '1';
         wait for 10 ns;
@@ -76,7 +78,7 @@ begin
         wait for 10 ns;
         wait for 10 ns;
 
-        t_addr <= X"02";
+        t_write_addr <= X"02";
         t_data_in <= X"dd";
         t_write_en <= '1';
         wait for 10 ns;
@@ -86,12 +88,12 @@ begin
         wait for 10 ns;
         wait for 10 ns;
 
-        t_addr <= X"01";
+        t_write_addr <= X"01";
         wait for 10 ns;
 
         t_write_en <= '1';
         for i in 0 to 31 loop 
-            t_addr <= to_unsigned(i,8);
+            t_write_addr <= to_unsigned(i,8);
             t_data_in <= to_unsigned(i+10,8);
             wait for 20 ns;
         end loop;
@@ -99,7 +101,7 @@ begin
         t_write_en <= '0';
         wait for 30 ns;
         for i in 0 to 31 loop 
-            t_addr <= to_unsigned(i,8);
+            t_read_addr <= to_unsigned(i,8);
             wait for 20 ns;
         end loop;
 
