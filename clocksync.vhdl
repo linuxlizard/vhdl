042734e4cbe8ac748ea2ae942fed7dfe9d464eb5
David Poole <testcluster@gmail.com>
2012-11-22 13:36:28 -0700

    change wait times to a constant

--- clocksync.vhdl
+++ clocksync.vhdl
@@ -14,6 +14,11 @@ entity clocksync is
 end entity;
 
 architecture clock_sync_test_arch of clocksync is
+    constant clk1_cycle : time := 5 ns;
+    constant clk2_cycle : time := 7 ns;
+--    constant clk1_cycle : time := 7 ns;
+--    constant clk2_cycle : time := 5 ns;
+
     component d_ff is
         port (clk : in std_logic;
                 reset : in std_logic;
@@ -35,14 +40,14 @@ architecture clock_sync_test_arch of clocksync is
 begin
     clock1 : process is
     begin
-       clk1 <= '0'; wait for 5 ns;
-       clk1 <= '1'; wait for 5 ns;
+       clk1 <= '0'; wait for clk1_cycle;
+       clk1 <= '1'; wait for clk1_cycle;
     end process clock1;
 
     clock2 : process is
     begin
-       clk2 <= '0'; wait for 7 ns;
-       clk2 <= '1'; wait for 7 ns;
+       clk2 <= '0'; wait for clk2_cycle;
+       clk2 <= '1'; wait for clk2_cycle;
     end process clock2;
 
     source_clk_ff : d_ff
@@ -68,16 +73,20 @@ begin
     begin
         write( str, string'("hello, world") );
         writeline( output, str );
-        data_in <= X"aa";
-        wait for 10 ns;
+        wait for clk1_cycle;
 
         reset <= '0';
+        wait for clk1_cycle;
+
+        data_in <= X"aa";
+        wait for 2*clk1_cycle;
+
         data_in <= X"bb";
-        wait for 10 ns;
+        wait for 2*clk1_cycle;
 
         for i in 1 to 10 loop
             data_in <= std_logic_vector(to_unsigned(i,8));
-            wait for 10 ns;
+            wait for 2*clk1_cycle;
 --            wait for i*10 ns;
 
 --            data_in <= std_logic_vector(to_unsigned(i*2,8));
@@ -85,7 +94,7 @@ begin
         end loop;
 
         data_in <= x"ee";
-        wait for 10 ns;
+        wait for 2*clk1_cycle;
 
         report "test done";  
         wait;
